c4eed995fcdc7b90a898d8af972ee41909175b4e
sbtree <sunbo0422@gmail.com>
2015-09-05 01:00:34 +0200

    HistroyChar, HistroyStr, HistroyHex in TCharBuffer

--- Delphi/General/DataBuffer.pas
+++ Delphi/General/DataBuffer.pas
@@ -10,6 +10,7 @@ type
     p_read  : Integer; //pointer to read
     p_write : Integer; //pointer to write
     b_overlap: Boolean; //indicate if write-pointer oversteps reader-pointer
+    b_ringed: Boolean; //indicate if p_write is already gone once around
   protected
     function ForwardRead(): boolean;
     function ForwardWrite(): boolean;
@@ -35,7 +36,6 @@ type
 
   protected
     function IsHexString(const str: string): boolean;
-    function WriteHexStr(const str: string): integer;
 
   public
     constructor Create;
@@ -43,9 +43,14 @@ type
 
     function Resize(const n: Integer):Boolean; override;
     function ReadChar(var ch: char): boolean;
-    function ReadStr(const bHex: boolean = false): String;
-    function WriteChar(ch: char): boolean;
-    function WriteStr(const str: string; const bHex: boolean = false): integer;
+    function ReadStr(): string;
+    function ReadHex(): string;
+    function HistoryChar(var ch: char; const idx: integer): boolean;
+    function HistoryStr(): string;
+    function HistoryHex(): string;
+    function WriteChar(const ch: char): boolean;
+    function WriteStr(const str: string): integer;
+    function WriteHex(const str: string): integer;
   end;
 
 const
@@ -66,6 +71,7 @@ begin
   result := false;
   if not b_overlap then begin
     inc(p_write);
+    if not b_ringed then b_ringed := (p_write >= i_size);  
     p_write := (p_write mod i_size);
     b_overlap := (p_write = p_read);
     result := true;
@@ -79,6 +85,7 @@ begin
   p_read  := 0;
   p_write := 0;
   b_overlap := false;
+  b_ringed:= false;
 end;
 
 destructor TBufferBase.Destroy;
@@ -127,26 +134,6 @@ begin
     end;
 end;
 
-function TCharBuffer.WriteHexStr(const str: string): integer;
-var i, iLen: integer; sBuf, sHex: string;
-begin
-  result := 0;
-  if IsHexString(str) then begin
-    iLen := length(str);
-    if (iLen mod 2) <> 0 then sBuf := '0' + str
-    else sBuf := str;
-    iLen := (length(sBuf) shr 1);
-    result := iLen;
-    for i := 1 to iLen  do begin
-      sHex := '$' + sBuf[i*2-1] + sBuf[i*2];
-      if not WriteChar(Char(StrToInt(sHex))) then begin
-        result := i;
-        break;
-      end;
-    end;
-  end;
-end;
-
 constructor TCharBuffer.Create();
 begin
   inherited Create;
@@ -182,15 +169,53 @@ begin
   end;
 end;
 
-function TCharBuffer.ReadStr(const bHex: boolean): String;
+function TCharBuffer.ReadStr(): string;
 var ch: char;
 begin
   result := '';
-  if bHex then while (ReadChar(ch)) do result := result + format('%.02x',[integer(ch)])
-  else while (ReadChar(ch)) do result := result + ch;
+  while (ReadChar(ch)) do result := result + ch;
 end;
 
-function TCharBuffer.WriteChar(ch: char): boolean;
+function TCharBuffer.ReadHex(): string;
+var ch: char;
+begin
+  result := '';
+  while (ReadChar(ch)) do result := result + format('%.02x',[integer(ch)])
+end;
+
+function TCharBuffer.HistoryChar(var ch: char; const idx: integer): boolean;
+var p_char: integer;
+begin
+  if b_ringed then  result := ((idx > 0) and (idx <= CountFree()))
+  else  result := ((idx > 0) and (idx <= p_read));
+
+  if result then begin
+    p_char := ((p_read + i_size - idx) mod i_size);
+    ch := a_chars[p_char];
+  end;
+end;
+
+function TCharBuffer.HistoryStr(): string;
+var ch: char; i: integer;
+begin
+  result := ''; i := 1;
+  while HistoryChar(ch, i) do begin
+    result := ch + result;
+    inc(i);
+  end;
+end;
+
+function TCharBuffer.HistoryHex(): string;
+var ch: char; i: integer;
+begin
+  result := ''; i := 1;
+  while HistoryChar(ch, i) do begin
+    result := format('%.02x', [integer(ch)]) + result;
+    inc(i);
+  end;
+end;
+
+function TCharBuffer.WriteChar(const ch: char): boolean;
 begin
   result := (not IsFull());
   if result then
@@ -200,13 +225,32 @@ begin
   end;
 end;
 
-function TCharBuffer.WriteStr(const str: string; const bHex: boolean): integer;
+function TCharBuffer.WriteStr(const str: string): integer;
 var i,iLen: integer;
 begin
   result := 0;
   iLen := length(str);
-  if bHex then result := WriteHexStr(str)
-  else for i := 1 to iLen do if (WriteChar(str[i])) then inc(result);
+  for i := 1 to iLen do begin
+    if (WriteChar(str[i])) then result := i
+    else break;
+  end;
+end;
+
+function TCharBuffer.WriteHex(const str: string): integer;
+var i, iLen: integer; sBuf, sHex: string;
+begin
+  result := 0;
+  if IsHexString(str) then begin
+    iLen := length(str);
+    if (iLen mod 2) <> 0 then sBuf := '0' + str
+    else sBuf := str;
+    iLen := (length(sBuf) shr 1);
+    for i := 1 to iLen  do begin
+      sHex := '$' + sBuf[i*2-1] + sBuf[i*2];
+      if WriteChar(Char(StrToInt(sHex))) then result := i
+      else break;
+    end;
+  end;
 end;
 
 end.
--- Delphi/General/GTMain.pas
+++ Delphi/General/GTMain.pas
@@ -27,17 +27,31 @@ procedure TForm2.btnTestClick(Sender: TObject);
 var chbuffer: TCharBuffer;  t: cardinal; n:integer;
 begin
   chbuffer := TCharBuffer.Create();
+  chbuffer.Resize(10);
   t:=GetTickCount;
-  n:=chbuffer.WriteStr('19084091483758913478faf214f',true);
+  n:=chbuffer.WriteHex('19084091483758913478faf214f');
   //chbuffer.Resize(14);
   t:=GetTickCount - t;
-  ShowMessage(format('n=%d, t=%d, CountUsed=%d', [n,t,chbuffer.CountUsed()]));
-  ShowMessage(chbuffer.ReadStr(true));
+  ShowMessage(format('n=%d, t=%d, CountUsed=%d, CountFree=%d', [n,t,chbuffer.CountUsed(), chbuffer.CountFree()]));
+  ShowMessage(format('1.ReadHex=%s',[chbuffer.ReadHex()]));
+  ShowMessage(format('2.ReadHex=%s',[chbuffer.ReadHex()]));
+  ShowMessage(format('n=%d, t=%d, CountUsed=%d, CountFree=%d', [n,t,chbuffer.CountUsed(), chbuffer.CountFree()]));
+  ShowMessage(format('HistoryHex=%s', [chbuffer.HistoryHex()]));
   t:=GetTickCount;
   n:=chbuffer.WriteStr('987654321');
   t:=GetTickCount - t;
-  ShowMessage(format('n=%d, t=%d, CountUsed=%d', [n,t,chbuffer.CountUsed()]));
-  ShowMessage(chbuffer.ReadStr());
+  ShowMessage(format('n=%d, t=%d, CountUsed=%d, CountFree=%d', [n,t,chbuffer.CountUsed(), chbuffer.CountFree()]));
+  ShowMessage(format('1.ReadStr=%s',[chbuffer.ReadStr()]));
+  ShowMessage(format('2.ReadStr=%s',[chbuffer.ReadStr()]));
+  ShowMessage(format('n=%d, t=%d, CountUsed=%d, CountFree=%d', [n,t,chbuffer.CountUsed(), chbuffer.CountFree()]));
+  ShowMessage(format('HistoryStr=%s', [chbuffer.HistoryStr()]));
+
+
+  for n := $30 to $39 do chbuffer.WriteChar(char(n));
+  ShowMessage(format('ReadStr=%s',[chbuffer.ReadStr()]));
+  ShowMessage(format('HistoryStr=%s',[chbuffer.HistoryStr()]));
+  ShowMessage(format('HistoryHex=%s',[chbuffer.HistoryHex()]));
+
   FreeAndNil(chbuffer);
 end;
 
