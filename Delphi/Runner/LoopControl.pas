271c238af38168b67e26c51d9cd69de7282aabd6
sbtree <sunbo0422@gmail.com>
2016-08-08 16:27:04 +0200

    LoopControl

--- Delphi/Runner/LoopControl.pas
+++ Delphi/Runner/LoopControl.pas
@@ -5,10 +5,16 @@ uses Contnrs;
 
 type
   TLoopEntry = class
-
+  protected
+    c_timeout:cardinal;
+    s_endexp: string;
   public
     constructor Create();
     destructor Destroy(); override;
+
+    property Timeout: cardinal read c_timeout write c_timeout;
+    property EndCondition: string read s_endexp write s_endexp;
+
 
   end;
 
